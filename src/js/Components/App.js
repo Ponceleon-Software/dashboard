45bf2ce52eeaea321b4a3dab3a14081113727db6
Cesar Pérez <cesaraugp01@gmail.com>
2021-04-09 13:45:33 -0400

    close #10 🆕 Componente App ⚛✔

---
+++ src/js/Components/App.js
@@ -0,0 +1,48 @@
+import { ComponenteReactivo, CustomElement } from "../Utils/reactivity.js";
+import { Sign } from "../Views/Sign.js";
+
+const appElements = (config = {}) => {
+  return {
+    appContainer: CustomElement.create("div", {
+      className: "w-full h-full bg-blue-500",
+      id: "App",
+    }),
+    view: Sign.container,
+  };
+};
+
+function _App(elements) {
+  this.state = {
+    isLogged: false,
+    currentView: false,
+  };
+
+  this.element = elements.appContainer;
+  this.view = elements.view;
+
+  this.template = function () {
+    const state = JSON.parse(JSON.stringify(this.state));
+    const { isLogged, currentView } = state;
+    /*this.currentView = this.view;
+    this.isLogged= false; //PENDIENTE*/
+    return [this.view];
+  };
+}
+_App.prototype = Object.create(ComponenteReactivo.prototype);
+_App.prototype.constructor = _App;
+
+const App = (config = {}) => {
+  const elements = appElements(config);
+  elements.appContainer.appendChild(elements.view);
+
+  const component = new _App(elements);
+  component.render();
+
+  return {
+    container: () => elements.appContainer,
+    elements: () => elements,
+    changeView: (view) => component.setState({ view: view }),
+    get: (elementName) => elements[elementName],
+  };
+};
+export { App };
